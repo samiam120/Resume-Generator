f7194a959167c58f72d3f9b9c4d25233510925d1
samiam120 <samassefa3@gmail.com>
2025-03-23 10:41:13 -0700

    basic component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,20 @@
-import { GeneralInfo } from "./GeneralInfo"
+import { GeneralInfo } from "./GeneralInfo";
+import { Summary } from "./Summary";
+import { Education } from "./Education";
+import { Experience } from "./Experience";
+import {Skills} from "./Skills";
+import "./styles.css";
 
 function App() {
-
   return (
-    <>
-    <form action="">
-      <label htmlFor="name">Name</label>
-      <input type="text" id="name" />
-      <label htmlFor="email">email</label>
-      <input type="text" id="email" />
-      <label htmlFor="phone">Phone</label>
-      <input type="tel" id="phone"/>
-    </form>
-    <GeneralInfo/>
-    <div className="summary">
-      <h2>SUMMARY</h2>
-      <p>Summary</p>
+    <div className="container">
+      <GeneralInfo name="John Doe" email="JohnDoe@gmail.com" phone="123-456-7890"/>
+      <Summary/>
+      <Education/>
+      <Experience/> 
+      <Skills/>
     </div>
-
-    <div className="education">
-      <h2>EDUCATION</h2>
-      <p>Education</p>
-    </div>
-
-    <div className="experience">
-      <h2>EXPERIENCE</h2>
-      <div className="job">
-
-      </div>
-    </div>
-
-    <div className="skills">
-      <h2>SKILLS</h2>
-      <p>Skills</p>
-    </div>
-
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
