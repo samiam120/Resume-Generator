e395a08c3dc5566586c8edaba40233bb08a9592d
samiam120 <samassefa3@gmail.com>
2025-03-26 14:58:11 -0700

    removing files

--- src/Skills.jsx
+++
@@ -1,9 +0,0 @@
-export function Skills(props){
-    return(
-        <div className="skills">
-            <h2>SKILLS</h2>
-            <hr />
-            <p>{!props.skills ? "Skills" : props.skills}</p>
-        </div>
-    )
-}
