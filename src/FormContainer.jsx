c6f837619c7a8c7d3a8c50fea4d2ce1819823faf
samiam120 <samassefa3@gmail.com>
2025-03-26 11:01:31 -0700

    controlled input for generalInformation

---
+++ src/FormContainer.jsx
@@ -0,0 +1,49 @@
+export function FormContainer() {
+  return <div className="form-container"></div>;
+}
+
+export function GeneralInformationForm({ generalInfo, setGeneralInfo }) {
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setGeneralInfo((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+  return (
+    <div className="">
+      <label htmlFor="form-name">name</label>
+      <input
+        type="text"
+        id="form-name"
+        name="name"
+        value={generalInfo.name}
+        onChange={handleChange}
+      />
+      <label htmlFor="form-email">email</label>
+      <input
+        type="email"
+        name="email"
+        id="form-email"
+        value={generalInfo.email}
+        onChange={handleChange}
+      />
+      <label htmlFor="form-phone">phone</label>
+      <input
+        type="tel"
+        id="form-phone"
+        name="phone"
+        value={generalInfo.phone}
+        onChange={handleChange}
+      />
+      <label htmlFor="form-linkedIn">LinkedIn</label>
+      <input
+        type="text"
+        id="form-linkedIn"
+        name="linkedIn"
+        value={generalInfo.linkedIn}
+        onChange={handleChange}
+      />
+    </div>
+  );
+}
